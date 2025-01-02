679ba8fbc60f12946b6a352ee396d91c342e09ca
Sai vamshi Gannoju <saivamshig404@gmail.com>
2025-01-02 18:44:33 +0530

    some more changes

--- src/app/test/insert.js
+++ src/app/test/insert.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 import { execute } from "./sql.js";
 
 const main = async () => {
-  const db = new sqlite3.Database("my.db");
+  const db = new sqlite3.Database("test");
   const sql = `INSERT INTO products(name, price) VALUES(?, ?)`;
   try {
     await execute(db, sql, ["iPhone", 899.99]);
