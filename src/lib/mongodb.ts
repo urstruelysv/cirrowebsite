e1d8ee733be5d4788bb5d0ee0b37cf695e22294e
Sai vamshi Gannoju <saivamshig404@gmail.com>
2025-01-28 20:57:59 +0530

    init

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+type CachedType = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+} | null;
+
+declare global {
+  var mongoose: CachedType;
+}
+
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/cirro";
 
@@ -7,13 +16,17 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = global.mongoose;
+let cached: CachedType = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
+  if (!cached) {
+    cached = { conn: null, promise: null };
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
@@ -23,9 +36,7 @@ async function connectDB() {
       bufferCommands: true,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI, opts);
   }
 
   try {
