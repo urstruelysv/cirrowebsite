9dc01ac19731537754b192e11e3420f3082890c6
Sai vamshi Gannoju <saivamshig404@gmail.com>
2025-03-19 00:43:08 +0530

    rigix fix

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -6,7 +6,7 @@ export const distributorshipApplicationSchema = z.object({
   mobile: z.string().regex(/^\d{10}$/, "Mobile number is invalid"),
   pincode: z
     .string()
-    .regex(/^[1-9]{1}\d{2}\s?\d{3}$/gm, "Pincode number is invalid"),
+    .regex(/^[1-9][0-9]{2}\s?[0-9]{3}$/, "Pincode number is invalid"),
   city: z.string().min(1, "City is required"),
   state: z.string().min(1, "State is required"),
 });
